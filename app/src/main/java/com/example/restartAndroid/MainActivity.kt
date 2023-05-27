6bc7cc2ecc94911a393ca6f0e803b46b0f8c648a
yangjinghua <1692047690@qq.com>
2023-05-27 23:42:22 +0800

    简单复习了一下生命周期

--- app/src/main/java/com/example/restartAndroid/MainActivity.kt
+++ app/src/main/java/com/example/restartAndroid/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.restartAndroid
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import android.view.View
 import android.widget.TextView
@@ -11,14 +12,36 @@ class MainActivity : AppCompatActivity(){
     val resultCode = 0x0001
     var cli:TextView? = null
     val TAG:String = "RESTART"
+    val saveKey = "savekey"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        取得保存数据
+        if (savedInstanceState!=null){
+            val template = savedInstanceState.getString(saveKey)
+            Log.e(TAG, "onCreate: ${template}", )
+        }
         cli = findViewById(R.id.cli)
         cli?.setOnClickListener(View.OnClickListener {
             gotoNextActivitygetResult()
         })
     }
+
+    /**
+     * 离开页面保存数据
+     */
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        val template = "保存的数据"
+        outState.putString(saveKey,template)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+    }
+
+
+
 // intent 携带返回值，这个问题忘记了重温
     fun gotoNextActivitygetResult(){
         val intent = Intent(this,MainActivity2::class.java);
--- app/src/main/java/com/example/restartAndroid/MainActivity2.java
+++ app/src/main/java/com/example/restartAndroid/MainActivity2.java
@@ -12,6 +12,7 @@ public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+//        这个方法在活动第一次被创建的时候调用，你应该在这个方法总完成活动的初始化操作比如加载布局绑定事件等
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         back = findViewById(R.id.back);
@@ -24,5 +25,41 @@ public class MainActivity2 extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+//        这个方法在活动由不可见变为可见的时候调用
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+//        这个方法在活动在活动准备好和用户进行交互调用此时活动一定位于返回栈的栈顶，并且处于运行状态
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+//        这个方法在系统准备去启动或者恢复另一个活动的时候调用，我们通常会在这个方中将一些消耗cpu的资源释放掉，以及保存一些相关数据，但这个方法的执行速度一定要快不然会影响到新的栈顶活动使用
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+//  这个方法在活动完全不可见的时候调用，她和onpause方法的主要区别在于如果启动的新活动是一个对话框那么onpause方法会的到执行而onstop不会
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+//        这个方法在活动被销毁之前调用，之后活动的状态将变为销毁状态
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onRestart() {
+//        这个方法在活动由停止状态变为运行状态之前调用也就是活动重新启动了。
+        super.onRestart();
     }
 }
